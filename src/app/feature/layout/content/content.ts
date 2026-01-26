95d52536b1d9b97f264548b2e8a9793349469bde
rukhsana1827 <rukhsana1827asif@gmail.com>
2026-01-26 11:58:59 +0530

    added whatsapp

--- src/app/feature/layout/content/content.ts
+++ src/app/feature/layout/content/content.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
 import { RouterOutlet } from '@angular/router';
+import { Whatsapp } from '../whatsapp/whatsapp';
 
 @Component({
   selector: 'app-content',
   imports: [Header,
     Footer,
+    Whatsapp,
     RouterOutlet
   ],
   templateUrl: './content.html',
---
+++ src/app/feature/layout/whatsapp/whatsapp.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whatsapp',
+  imports: [],
+  templateUrl: './whatsapp.html',
+  styleUrl: './whatsapp.css',
+})
+export class Whatsapp {
+
+}
