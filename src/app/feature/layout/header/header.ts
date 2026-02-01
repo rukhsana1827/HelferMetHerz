ceeb06a5cf1732a179f74e5a30172056804e62d9
rukhsana1827 <rukhsana1827asif@gmail.com>
2026-02-01 17:06:51 +0530

    updated header fixed state

--- src/app/feature/layout/header/header.ts
+++ src/app/feature/layout/header/header.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    CommonModule
   ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  isMegaMenuOpen = false;
+
+  closeMegaMenu() {
+    this.isMegaMenuOpen = false;
+  const dropdown = document.querySelector('.mega-menu-content') as HTMLElement;
+  if (dropdown) {
+    dropdown.style.display = 'none';
+    dropdown.style.opacity = '0';
+    dropdown.style.visibility = 'hidden';
+  }
+}
+openMegaMenu() {
+  this.isMegaMenuOpen = true;
+  const dropdown = document.querySelector('.mega-menu-content') as HTMLElement;
+  if (dropdown) {
+    dropdown.style.display = 'block';
+    dropdown.style.opacity = '1';
+    dropdown.style.visibility = 'visible';
+  }
+}
+@HostListener('document:click', ['$event'])
+onDocumentClick(event: MouseEvent) {
+  const target = event.target as HTMLElement;
+  const clickedInside = target.closest('.mega-menu-static');
+
+  if (!clickedInside && this.isMegaMenuOpen) {
+    this.closeMegaMenu();
+  }
+}
 
 }
