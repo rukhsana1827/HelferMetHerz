daa8fb01bd2dbbf98b4f6ef5b9f0ba25f5bb6229
rukhsana1827 <rukhsana1827asif@gmail.com>
2026-02-01 19:27:17 +0530

    updated header navigations

--- src/app/feature/layout/header/header.ts
+++ src/app/feature/layout/header/header.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -13,33 +13,45 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class Header {
   isMegaMenuOpen = false;
+  @ViewChild('navbarCollapse') navbarCollapse!: ElementRef;
+
+
+  closeFullNavbar() {
+    // 1. Close the Mega Menu dropdown specifically
+    this.isMegaMenuOpen = false;
+
+    // 2. Close the main Bootstrap Mobile Navbar
+    if (this.navbarCollapse.nativeElement.classList.contains('show')) {
+      this.navbarCollapse.nativeElement.classList.remove('show');
+    }
+  }
 
   closeMegaMenu() {
     this.isMegaMenuOpen = false;
-  const dropdown = document.querySelector('.mega-menu-content') as HTMLElement;
-  if (dropdown) {
-    dropdown.style.display = 'none';
-    dropdown.style.opacity = '0';
-    dropdown.style.visibility = 'hidden';
+    const dropdown = document.querySelector('.mega-menu-content') as HTMLElement;
+    if (dropdown) {
+      dropdown.style.display = 'none';
+      dropdown.style.opacity = '0';
+      dropdown.style.visibility = 'hidden';
+    }
   }
-}
-openMegaMenu() {
-  this.isMegaMenuOpen = true;
-  const dropdown = document.querySelector('.mega-menu-content') as HTMLElement;
-  if (dropdown) {
-    dropdown.style.display = 'block';
-    dropdown.style.opacity = '1';
-    dropdown.style.visibility = 'visible';
+  openMegaMenu() {
+    this.isMegaMenuOpen = true;
+    const dropdown = document.querySelector('.mega-menu-content') as HTMLElement;
+    if (dropdown) {
+      dropdown.style.display = 'block';
+      dropdown.style.opacity = '1';
+      dropdown.style.visibility = 'visible';
+    }
   }
-}
-@HostListener('document:click', ['$event'])
-onDocumentClick(event: MouseEvent) {
-  const target = event.target as HTMLElement;
-  const clickedInside = target.closest('.mega-menu-static');
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const clickedInside = target.closest('.mega-menu-static');
 
-  if (!clickedInside && this.isMegaMenuOpen) {
-    this.closeMegaMenu();
+    if (!clickedInside && this.isMegaMenuOpen) {
+      this.closeMegaMenu();
+    }
   }
-}
 
 }
